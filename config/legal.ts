81e598d71ba7f19a0d54c3cae09b1ad18c38da6e
WojciechWnek <wownek@gmail.com>
2026-06-01 22:16:46 -0500

    feat: add legal config with placeholder company data

---
+++ config/legal.ts
@@ -0,0 +1,8 @@
+export const legal = {
+  companyName: "Nutri Scale",
+  appName: "Nutri Scale",
+  email: "contact@nutriscale.com",
+  address: "123 Nutrition Street, Food City, FC 12345",
+  domain: "nutriscale.com",
+  effectiveDate: "June 1, 2026",
+} as const;
